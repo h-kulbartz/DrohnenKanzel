91172350f1f4a129b0b148892a0a2fd1751e9593
Hermann <hermann@Desktop.localdomain>
2017-09-12 20:45:05 +0200

    Initial Commit Drohnenkanzel

---
+++ Crazyflie_test.py
@@ -0,0 +1,263 @@
+# -*- coding: utf-8 -*-
+#
+#     ||          ____  _ __
+#  +------+      / __ )(_) /_______________ _____  ___
+#  | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+#  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+#   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+#
+#  Copyright (C) 2014 Bitcraze AB
+#
+#  Crazyflie Nano Quadcopter Client
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA  02110-1301, USA.
+
+"""
+Simple example that connects to the first Crazyflie found, ramps up/down
+the motors and disconnects.
+"""
+
+import time
+import sys
+from threading import Thread
+import logging
+# sys.path.append(r"C:\Users\Hermann Kulbartz\Desktop\bitcraze\crazyflie-clients-python-2015.09\crazyflie-clients-python-2015.09\lib")
+import cflib  # noqa
+from cflib.crazyflie import Crazyflie  # noqa
+
+logging.basicConfig(level=logging.ERROR)
+
+
+class MotorRampExample():
+    """Example that connects to a Crazyflie and ramps the motors up/down and
+    than disconnects"""
+
+    def __init__(self, link_uri):
+        """ Initialize and run the example with the specified link_uri """
+
+        self._cf = Crazyflie()
+
+        self._cf.connected.add_callback(self._connected)
+        self._cf.disconnected.add_callback(self._disconnected)
+        self._cf.connection_failed.add_callback(self._connection_failed)
+        self._cf.connection_lost.add_callback(self._connection_lost)
+        self._cf.open_link(link_uri)
+        global gl_link_uri
+        gl_link_uri = link_uri
+        time.sleep(0.5)
+        print("Connecting to %s" % link_uri, "Crazyflie_test zeile 59")
+#        self._ramp_motors()
+#
+        thrust = 500
+     
+        print("In MotorRampExample() thrust %i" % thrust)
+#        self.up_motors(thrust)
+#        self.motors()
+
+    def _connected(self, link_uri):
+        """ This callback is called form the Crazyflie API when a Crazyflie
+        has been connected and the TOCs have been downloaded."""
+
+        # Start a separate thread to do the motor test.
+        # Do not hijack the calling thread!
+        print ("In connected zeile 74")
+        Thread(target=self._ramp_motors).start()
+#
+#        Thread(target=self.up_motors).start()
+#        Thread(target=self.motors).start()
+
+    def _connection_failed(self, link_uri, msg):
+        """Callback when connection initial connection fails (i.e no Crazyflie
+        at the specified address)"""
+        print("Connection to %s failed: %s" % (link_uri, msg))
+
+    def _connection_lost(self, link_uri, msg):
+        """Callback when disconnected after a connection has been made (i.e
+        Crazyflie moves out of range)"""
+        print("Connection to %s lost: %s" % (link_uri, msg))
+
+    def _disconnected(self, link_uri):
+        """Callback when the Crazyflie is disconnected (called in all cases)"""
+        print("Disconnected from %s" % link_uri, "Crazyflie_test zeile 92")
+
+    def _ramp_motors(self):
+        thrust_mult = 1
+        thrust_step = 500
+        thrust = 20000
+        pitch = 0
+        roll = 0
+        yawrate = 0
+
+        # Unlock startup thrust protection
+        self._cf.commander.send_setpoint(0, 0, 0, 0)
+
+        while thrust >= 10000:
+            self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
+            time.sleep(0.5)
+            if thrust >= 40000:
+                thrust_mult = -1
+            thrust += thrust_step * thrust_mult
+            print(thrust)
+        self._cf.commander.send_setpoint(0, 0, 0, 0)
+        time.sleep(0.5)
+        
+        
+    def up_motors(self, thrust):
+        pitch = 0
+        roll = 0
+        yawrate = 0
+#        self._cf = Crazyflie()
+#        thrust = self.thrust        
+#        thrust = 20000
+        self._cf.commander.send_setpoint(0, 0, 0, 0)
+
+#        thrust = self.thrust
+        print("Aus Crazyflie_test Zeile 126", thrust)
+        print(type(thrust))
+        pitch = 0
+        roll = 0
+        yawrate = 0
+        self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
+#        while thrust >= 10000:
+#            self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
+#        thrust = 40000
+#        self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
+#        time.sleep(0.5)
+#        self._cf.commander.send_setpoint(0, 0, 0, 0)
+#        thrust = 40000
+#        self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
+#        time.sleep(3.)
+#        thrust=43000
+#        pitch=0.
+#        thrust = 4500
+        for i in range(5):
+            print("Ramp up %3i"%pitch, thrust)
+            self._cf.commander.send_setpoint(0, -30, 0, thrust)
+            pitch += 50
+            time.sleep(1)
+            print("Ramp down")
+            self._cf.commander.send_setpoint(0, 0, 0, thrust)
+            time.sleep(1)
+            # Make sure that the last packet leaves before the link is closed
+            # since the message queue is not flushed before closing
+        time.sleep(0.1)
+#        self._cf.close_link()
+#        print("Schliesse Link")
+        time.sleep(0.1)
+        
+    def motorsroll(self, roll):
+        print("Zeile 162 roll = ", roll)
+        nthrust = 0
+        npitch = 0
+        nroll = 0
+        nyawrate = 0
+        self._cf.commander.send_setpoint(nroll, npitch, nyawrate, nthrust)
+        time.sleep(0.1)
+        self._cf.commander.send_setpoint(roll, 0, 0, 0)
+        time.sleep(0.1)
+
+    def motorspitch(self, pitch):
+        print("Zeile 173 pitch = ", pitch)
+        nthrust = 0
+        npitch = 0
+        nroll = 0
+        nyawrate = 0
+        self._cf.commander.send_setpoint(nroll, npitch, nyawrate, nthrust)
+        time.sleep(0.1)
+        self._cf.commander.send_setpoint(0, pitch, 0, 0)
+        time.sleep(0.1)
+        
+    def motorsyawrate(self, yawrate):
+        nthrust = 0
+        npitch = 0
+        nroll = 0
+        nyawrate = 0
+        self._cf.commander.send_setpoint(nroll, npitch, nyawrate, nthrust)
+        time.sleep(0.1)
+        self._cf.commander.send_setpoint(0, 0, yawrate, 0)
+        time.sleep(0.5)        
+        
+    def motorsthrust(self, thrust):
+#        nthrust = 0
+#        npitch = 0
+#        nroll = 0
+#        nyawrate = 0
+#        thrust_mult = 1
+#        self._cf.commander.send_setpoint(nroll, npitch, nyawrate, nthrust)
+#        time.sleep(0.1)
+        self._cf.commander.send_setpoint(0, 0, 0, thrust)
+#        while thrust >= 10000:
+#            self._cf.commander.send_setpoint(0, 0, 0, thrust)
+#            time.sleep(0.5)
+#            if thrust >= 20000:
+#                thrust_mult = -1
+#            thrust += 3000 * thrust_mult
+#            print(thrust)
+        print("thrust = ", thrust)
+        time.sleep(0.5)
+        
+    def Close(self):
+        time.sleep(0.1)
+        self._cf.close_link()
+        print("Schliesse Link")
+        time.sleep(0.1)
+
+    def Open(self):
+        ScanCfly()        
+        time.sleep(0.1)
+        self._cf.open_link(gl_link_uri)
+        print("Öffne     Link")
+        time.sleep(0.1)        
+        
+def ScanCfly():
+    # Initialize the low-level drivers (don't list the debug drivers)
+    cflib.crtp.init_drivers(enable_debug_driver=False)
+    # Scan for Crazyflies and use the first one found
+    print("(2) Scanning interfaces for Crazyflies...")
+    available = cflib.crtp.scan_interfaces()
+    print("(2) Crazyflies found:")
+    l = 1
+    for i in available:
+        print(l, i[0])
+        l += 1
+
+    if len(available) > 0:
+        le = MotorRampExample(available[0][0])
+        print(l, "(2) After le", le)
+        print()
+    else:
+        print("(2) No Crazyflies found, cannot run example")
+    
+#    return le
+
+if __name__ == '__main__':
+    # Initialize the low-level drivers (don't list the debug drivers)
+    cflib.crtp.init_drivers(enable_debug_driver=False)
+    # Scan for Crazyflies and use the first one found
+    print("(1) Scanning interfaces for Crazyflies...")
+    available = cflib.crtp.scan_interfaces()
+    print("(1) Crazyflies found:")
+    for i in available:
+        print(i[0])
+
+    if len(available) > 0:
+        le = MotorRampExample(available[0][0])
+        print("(1) After le", le)
+    else:
+        print("(1) No Crazyflies found, cannot run example")
+else:
+    print("Library call")    
+
---
+++ Drohnen_Kanzel.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+/*****************************************************************************/
+/* Created on Fri Aug 11 19:54:49 2017                                       */
+/* Type        : Python Script                                               */
+/* Description :                                                             */
+/* Module      :                                                             */
+/* Resources   : time, os, csv und stat.                                     */ 
+/* Input       : [Laufwerk] default "F",                                     */
+/* Output      : F:/10Admin/Data/CSV/letzter Zugriff_[end]_[laufw].csv,      */
+/*                                                                           */
+/* @author     : Hermann Kulbartz                                            */
+/*****************************************************************************/
+"""
+print(__doc__)
+from tkinter import *
+import sys
+print("vOR IMPORT")
+import Crazyflie_test 
+# print(dir(Crazyflie_test))
+print("Nach Import")
+global M
+global count
+
+M = Crazyflie_test.ScanCfly()
+print(type(M))
+# sys.exit(1)
+
+
+count = 0
+thrust = 0
+def mouse_wheel(event):
+    
+    # respond to Linux or Windows wheel event
+    if event.num == 5 or event.delta == -120:
+        count -= 500
+    if event.num == 4 or event.delta == 120:
+        count += 500
+    label['text'] = count #int(var1.get())
+    #thrust = count
+    #int(var1,get()) = count
+
+def sel():
+   selection1 = "thrust = " + str(var1.get()) + "  yawrate = " + str(var2.get()) + " °"
+   label1.config(text = selection1)
+   selection2 = "roll = " + str(var3.get()) + " °" + "  pitch = " + str(var4.get()) + " °"
+   label2.config(text = selection2)
+   print(selection1, selection2)
+   
+   thrust = count  #int(var1.get())
+   print(thrust)
+   M.motorsthrust(thrust)
+#
+#   yawrate = int(var2.get())
+#   M.motorsyawrate(yawrate)
+#   
+#   roll = int(var3.get())
+#   M.motorsroll(roll)
+#   
+#   pitch = int(var2.get())
+#   M.motorspitch(pitch)
+
+def MotorThrust(count):
+   thrust = count  #int(var1.get())
+   print(thrust)
+   M.motorsthrust(thrust)
+   
+def Close():
+   M.Close()
+   
+def Open():
+   M.Open()
+   
+root = Tk()
+root.title("Drohnen Kanzel")
+var1 = IntVar()
+var2 = IntVar()
+var3 = IntVar()
+var4 = IntVar()
+
+scale1 = Scale( root, variable = var1, from_ = 60000, to = 0, length = 200, tickinterval = 10000)
+scale1.pack(side = RIGHT)
+scale1.set(9000)
+
+scale4 = Scale( root, variable = var4, from_ = 180, to = -180, length = 200, tickinterval = 90 )
+scale4.pack(side = LEFT)
+
+
+scrollbar = Scrollbar(root)
+scrollbar.pack( side = RIGHT, fill=Y )
+
+root.bind("<Button-4>", mouse_wheel)
+root.bind("<Button-5>", mouse_wheel)
+label = Label(root, font=('courier', 12, 'bold'), width=10)
+label.pack(padx=80, pady=40)
+
+
+#scale4 = Scale( root, variable = var4, from_ = 180, to = -180, length = 400, tickinterval = 90 )
+#scale4.pack(side = LEFT)
+
+
+scale2 = Scale( root, variable = var2, from_ = -180, to = 180, length = 600, tickinterval=90, orient=HORIZONTAL )
+scale2.pack()
+
+scale3 = Scale( root, variable = var3, from_ = -180, to = 180, length = 600, tickinterval=90, orient=HORIZONTAL )
+scale3.pack()
+
+
+button1 = Button(root, text="Get Scale Values", command=sel)
+button1.pack()
+
+button2 = Button(root, text="Close Link", bg = '#C1281F',command=Close)
+button2.pack(side = 'right')
+
+button3 = Button(root, text="Open Link", bg = 'green',command=Open)
+button3.pack(side = 'left')
+
+
+label1 = Label(root)
+label1.pack()
+
+label2 = Label(root)
+label2.pack()
+
+label3 = Label(root)
+label3.pack()
+
+label4 = Label(root)
+label4.pack()
+
+while True:
+    root.update_idletasks()
+    root.update() 
+    MotorThrust(count)
+#    time.sleep(0.5)
+#root.mainloop()
